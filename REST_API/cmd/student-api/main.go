7f4c4f6a44db0037091282b784de348cf39436ed
atomhudson <dhruvsaini0904@gmail.com>
2025-05-16 01:31:28 +0530

    graceful shutdown

--- REST_API/cmd/student-api/main.go
+++ REST_API/cmd/student-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/atomhudson/go_lang/student-api/internal/config"
 )
@@ -23,10 +29,33 @@ func main() {
 		Addr:    cfg.HTTPServer.Addr,
 		Handler: router,
 	}
+
+	slog.Info("starting server...", slog.String("address", cfg.HTTPServer.Addr))
 	fmt.Println("starting server on", cfg.HTTPServer.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("failed to start server: %v", err.Error())
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, os.Kill)
+
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err.Error())
+		}
+	}()
+
+	<-done // wait for signal
+
+	slog.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
 
+	slog.Info("server stopped successfully")
 }
